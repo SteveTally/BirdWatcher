afb1257f319c40a0c3c1e633c7ccd8ffb9164736
Steven Tally <steventally@gmail.com>
2019-11-23 21:18:08 -0500

    Testing portrait aspect

--- TakePhoto.py
+++ TakePhoto.py
@@ -2,8 +2,8 @@
 from picamera import PiCamera
 import time
 
-camera =  PiCamera(sensor_mode=4)
-camera.resolution = (1640, 1232)
+camera =  PiCamera(sensor_mode=3)
+camera.resolution = (1080, 1920
 camera.start_preview()
 time.sleep(5)
 camera.capture('Output/test_image.jpg')
